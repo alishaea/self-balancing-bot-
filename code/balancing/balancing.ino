0c84a12aad11338f9532311ddd59ed542db234a9
alishaea <alishaeruma@gmail.com>
2026-09-11 21:48:21 +0530

    Add initial comments for balancing firmware
    Added documentation for self-balancing robot firmware.

--- code/balancing/balancing.ino
+++ code/balancing/balancing.ino
@@ -1,1 +1,23 @@
-
+/*
+ * Self-Balancing Robot
+ * --------------------
+ *
+ * This file is reserved for the balancing firmware
+ * used by the self-balancing robot.
+ *
+ * The original project firmware is currently unavailable.
+ * This repository documents the hardware, system architecture,
+ * mechanical design, and development of the robot.
+ *
+ * The balancing system was designed around:
+ * - MPU6050 IMU
+ * - Microcontroller
+ * - PID feedback control
+ * - L298N motor driver
+ * - Two DC motors
+ *
+ * NOTE:
+ * This file is intentionally not populated with reconstructed
+ * firmware so that the repository does not misrepresent
+ * reconstructed code as the original project implementation.
+ */
